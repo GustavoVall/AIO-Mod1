a6c6aefcb4e013fbae3f9e942e91aa1f4c601bd8
GustavoVall <gusvall.cyc@gmail.com>
2022-01-13 17:18:17 -0300

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<head>
+    <head>
         <title>
             STOP MISSING AND START KILLING
         </title>
@@ -12,8 +11,37 @@
         <link rel="stylesheet" type="text/css" href="css/responsivo.css">
 
     </head>
-</head>
-<body>
-    
-</body>
+
+    <body>
+
+    <?php
+    include('header.php')
+    ?>
+
+        <main>
+            <div class="container1">
+
+                <section>
+                    
+                    <h2 id="centrado" class="title30">
+                        STOP MISSING AND START KILLING
+                    </h2>
+
+                    <p>¿Cansado de no dar en el blanco? ¿Otra vez midio mal la distancia o el viento?</p>
+                    <p>No desespere, acertar al objetivo esta al alcance de su arma y su billetera.</p>
+                    <p>Esa sensación de frustracion esta a punto de desaparecer, solamente debe adherirse a alguno de nuestros cursos.</p>
+                    <p>Actualmente tenemos expertos alrededor del mundo para guiarlo, asistirlo (y cubrirlo en caso de necesidad).</p>
+                    <p>Solamente con completar la inscripción al curso y saber por cual lado del arma sale el proyectil estará listo para empezar.</p>
+                    <!-- EL CURSO DE SUICIDE BOMBER NO TIENE EJEMPLOS EN VIVO POR CAUSAS OBVIAS -->
+
+                </section>
+
+                <article id="video1">
+                <img src="Imagenes/missed.png">
+                </article>
+
+            </div>
+        </main>
+    </body>
+
 </html>
